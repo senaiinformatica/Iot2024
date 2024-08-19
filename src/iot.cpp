9e8ac2ee7e8b5d5a21b65e40c584087bb87d58fb
professorThiago <thiagomecatronicasenai@gmail.com>
2024-08-19 15:02:58 -0300

    EXERCICIO

--- src/iot.cpp
+++ src/iot.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <ArduinoJson.h> 
+#include <ArduinoJson.h>
+#include <TimeLib.h>
 #include "iot.h"
 #include "senhas.h"
 #include "saidas.h"
 #include "atuadores.h"
 
 // Definição dos tópicos de inscrição
-#define mqtt_topic1 "projetoProfessor/DHT"
+#define mqtt_topic1 "projeto_integrado/SENAI134/Cienciadedados/GrupoX"
 #define mqtt_topic2 "projetoProfessor/led2"
 #define mqtt_topic3 "projetoProfessor/servo"
-
 
 // Definição do ID do cliente MQTT randomico
 const String cliente_id = "ESP32Client" + String(random(0xffff), HEX);
@@ -102,45 +102,82 @@ void publica_mqtt(String topico, String msg)
 }
 
 //!----------------------------------------
-//TODO Alterar a programação apartir daqui
+// TODO Alterar a programação apartir daqui
 //!----------------------------------------
 
 // Inscreve nos tópicos MQTT
 void inscricao_topicos()
 {
-  client.subscribe(mqtt_topic1); //LED 1
-  client.subscribe(mqtt_topic2); //LED 2
-  client.subscribe(mqtt_topic3); //Servo
+  client.subscribe(mqtt_topic1); // LED 1
+  // client.subscribe(mqtt_topic2); //LED 2
+  // client.subscribe(mqtt_topic3); //Servo
 }
 
 // Trata as mensagens recebidas
 void tratar_msg(char *topic, String msg)
 {
   JsonDocument doc;
-  
+
+  //{"temperature":27.12,"pressure":93207.68,"altitude":753.96,"humidity":26,"CO2":0.82,"timestamp":1724077225}
+
   // Tratamento do tópico 1
   if (strcmp(topic, mqtt_topic1) == 0)
   {
     deserializeJson(doc, msg);
-    float umidade = doc["umidade"];
-    float temperatura = doc["temperatura"];
 
-    Serial.print("Umidade: ");
-    Serial.print(umidade);
-    Serial.print(" %\t");
-    Serial.print("Temperatura: ");
-    Serial.print(temperatura);
-    Serial.println(" *C"); 
+    float var_temperatura;
+    float var_pressao;
+    float var_altitude;
+    int var_umidade;
+    float var_co2;
+    unsigned long var_timestamp;
+
+    if (doc.containsKey("temperature"))
+    {
+      var_temperatura = doc["temperature"];
+    }
+
+    if (doc.containsKey("pressure"))
+    {
+      var_pressao = doc["pressure"];
+    }
+
+    if (doc.containsKey("altitude"))
+    {
+      var_altitude = doc["altitude"];
+    }
+
+    if (doc.containsKey("humidity"))
+    {
+      var_umidade = doc["humidity"];
+    }
+
+    if (doc.containsKey("CO2"))
+    {
+      var_co2 = doc["CO2"];
+    }
+
+    if (doc.containsKey("timestamp"))
+    {
+      var_timestamp = doc["timestamp"];
+    }
+
+    Serial.println("Temperatura: " + String(var_temperatura));
+    Serial.println("Pressão: " + String(var_pressao));
+    Serial.println("Altitude: " + String(var_altitude));
+    Serial.println("Umidade: " + String(var_umidade));
+    Serial.println("CO2: " + String(var_co2));
+    setTime(var_timestamp);
+    Serial.printf("%02d/%02d/%04d", day(), month(), year());
+    Serial.printf(" %02d:%02d:%02d \n", hour(), minute(), second());
   }
 
-  else   if (strcmp(topic, mqtt_topic2) == 0)
+  else if (strcmp(topic, mqtt_topic2) == 0)
   {
-    
   }
 
-   else if (strcmp(topic, mqtt_topic3) == 0)
+  else if (strcmp(topic, mqtt_topic3) == 0)
   {
     posiciona_servo(msg.toInt());
   }
-
 }
